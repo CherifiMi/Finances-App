27334b3a9daf41b2118cac64fb1d4595990bfcd6
CherifiMi <cherifi154949@gmail.com>
2022-04-07 20:08:49 +0100

    done

--- app/src/main/java/com/example/aimanmuneeb/MainActivity.kt
+++ app/src/main/java/com/example/aimanmuneeb/MainActivity.kt
@@ -9,14 +9,16 @@ import androidx.navigation.findNavController
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    var PACKAGE_NAME: String = packageName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         navController= findNavController(R.id.fragmentContainerView)
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
     }
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/aimanmuneeb/ui/Score.kt
+++ app/src/main/java/com/example/aimanmuneeb/ui/Score.kt
@@ -1,11 +1,16 @@
 package com.example.aimanmuneeb.ui
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.VideoView
 import com.example.aimanmuneeb.R
+import android.os.Environment
+import android.widget.MediaController
+import com.example.aimanmuneeb.MainActivity
 
 
 class Score : Fragment() {
@@ -14,7 +19,8 @@ class Score : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false)
+        val view = inflater.inflate(R.layout.fragment_score, container, false)
+
+        return view
     }
 }
